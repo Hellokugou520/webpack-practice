2de1034470c031c1a0641c587b49fdd33d1a119a
Bpoi <1378400943@qq.com>
2021-07-26 18:03:13 +0800

    js兼容性处理

---
+++ 13.js兼容性处理/webpack.config.js
@@ -0,0 +1,40 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'js/built.js',
+    path: resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [['@babel/preset-env', {
+            useBuiltIns: 'usage',
+            corejs: {
+              version: 3
+            },
+            targets: {
+              chrome: '60',
+              firefox: '60',
+              ie: '9',
+              safari: '10',
+              edge: '17'
+            }
+          }]]
+        }
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  mode: 'development',
+};
